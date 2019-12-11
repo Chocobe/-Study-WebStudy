fba5c9800309be871268c6400900a692ec59a31e
Chocobe <nice_10bird@naver.com>
2019-12-11 16:08:04 +0900

    <master> : Created package "sec02.ex01"

---
+++ WebStudy/pro08/src/sec02/ex01/FIrstServlet4.java
@@ -0,0 +1,24 @@
+package sec02.ex01;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first4")
+public class FIrstServlet4 extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("second4");
+		
+		dispatcher.forward(request, response);
+	}
+}
---
+++ WebStudy/pro08/src/sec02/ex01/SecondServlet4.java
@@ -0,0 +1,28 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/second4")
+public class SecondServlet4 extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+					throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<html>");
+		out.print("<body>");
+		out.print("dispatch를 이용한 forward 실습 입니다");
+		out.print("</body>");
+		out.print("</html>");
+		
+		out.close();
+	}
+}

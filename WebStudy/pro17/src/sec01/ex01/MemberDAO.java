001323b96213bb9fcaf42a75bc3ab2c3f1517b06
Chocobe <nice_10bird@naver.com>
2019-12-20 18:37:03 +0900

    <master> : Created "package sec01.ex01 & folder test01"

---
+++ WebStudy/pro17/src/sec01/ex01/MemberDAO.java
@@ -0,0 +1,113 @@
+package sec01.ex01;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	private DataSource dataSource;
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private static final String RESOURCE_DIR;
+	private static final String RESOURCE_NAME;
+	
+	static {
+		RESOURCE_DIR = "java:/comp/env";
+		RESOURCE_NAME = "jdbc/study/mysql";
+	}
+	
+	public MemberDAO() {
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context)initContext.lookup(RESOURCE_DIR);
+			dataSource = (DataSource)envContext.lookup(RESOURCE_NAME);
+			
+		} catch(NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public List<MemberVO> listMembers() {
+		List<MemberVO> membersList = new ArrayList<MemberVO>();
+		
+		try {
+			String sql = "SELECT * FROM T_MEMBER ORDER BY JOIN_DATE DESC";
+			
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String id = rs.getString("ID");
+				String pwd = rs.getString("PWD");
+				String name = rs.getString("NAME");
+				String email = rs.getString("EMAIL");
+				Date joinDate = rs.getDate("JOIN_DATE");
+				
+				MemberVO memberVO = new MemberVO(id, pwd, name, email, joinDate);
+				
+				membersList.add(memberVO);
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			close(conn, pstmt, rs);
+		}
+		
+		return membersList;
+	}
+	
+	
+	private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+		try {
+			if(rs != null) {
+				rs.close();
+				
+				close(conn, pstmt);
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void close(Connection conn, PreparedStatement pstmt) {
+		try {
+			if(pstmt != null) {
+				pstmt.close();
+				
+				close(conn);
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void close(Connection conn) {
+		try {
+			if(conn != null) {
+				conn.close();
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
